725efede7aa56e29506fedf2634b67c4ca425a75
TimothePure <94550676+TimothePure@users.noreply.github.com>
2023-11-17 10:23:49 +0100

    feat : fence destroy particles

--- Assets/Scripts/BulletWall.cs
+++ Assets/Scripts/BulletWall.cs
@@ -1,3 +1,4 @@
+using System;
 using Game.Script.SoundManager;
 using JetBrains.Annotations;
 using System.Collections;
@@ -11,13 +12,32 @@ public class BulletWall : MonoBehaviour
     private int _wallHP = 3;
     public int WallHP {  get { return _wallHP; }}
     public AudioClip _wallCrackAudio;
+
+    [SerializeField] public GameObject hitParticles;
     public void HitWall()
     {
         _wallHP--;
        if(!GameManager.instance._inputJuiciness._input2) ServiceLocator.Get().PlaySound(_wallCrackAudio, GameManager.instance._audioMixer);
+
+       GameObject particleInstance = Instantiate(hitParticles, transform.position, Quaternion.Euler(0,0,0));
+       StartCoroutine(WaitCoroutine(particleInstance));
     }
     private Animator _animator => this.GetComponent<Animator>();
 
     public Animator Animator { get => _animator;}
-    public void DisableSelf() => this.gameObject.SetActive(false);
+    //public void DisableSelf() => this.gameObject.SetActive(false);
+
+    IEnumerator WaitCoroutine(GameObject particleInstance)
+    {
+        if (particleInstance)
+        {
+            yield return new WaitForSeconds(1);
+            Destroy(particleInstance);
+        }
+
+        if (_wallHP < 1)
+        {
+            Destroy(this);
+        }
+    }
 }
